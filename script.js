de57b81d60d0b516fddf7b2cb80055c7738bed1e
Shekhar Dangi <arpitt682@gmail.com>
2024-04-14 11:33:31 +0530

    collision processed without animation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-console.log("Collision!");
+console.log("Collision on scale of 1px = 1m!");
+
 const canvas = document.querySelector("canvas");
 const tooltip = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+
+// Set canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 function getRandomColor() {
   return `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(
@@ -14,31 +17,58 @@ class Ball {
   constructor(x, vx, mass, radius) {
     this.x = x;
     this.vx = vx;
+    this.mass = mass;
     this.radius = radius;
     tooltip.fillStyle = getRandomColor();
+    tooltip.font = `${this.radius * 0.4}px Arial`;
   }
 
-  checkBoundaryCollision() {
+  collisionToWall() {
     return this.x <= this.radius || this.x + this.radius >= canvas.width;
+  }
+
+  collisionToBall(ball) {
+    return Math.abs(ball.x - this.x) <= ball.radius + this.radius;
+  }
+
+  distanceToBall(ball) {
+    return Math.abs(ball.x - this.x);
+  }
+  processCollision(ball) {
+    let final1 =
+      ((this.mass - ball.mass) / (this.mass + ball.mass)) * this.vx +
+      ((2 * ball.mass) / (this.mass + ball.mass)) * ball.vx;
+    let final2 =
+      ((2 * this.mass) / (this.mass + ball.mass)) * this.vx +
+      ((ball.mass - this.mass) / (this.mass + ball.mass)) * ball.vx;
+    this.vx = final1;
+    ball.vx = final2;
   }
 
   draw() {
     tooltip.beginPath();
     tooltip.arc(
       this.x,
-      canvas.height - this.radius - 1,
+      canvas.height - this.radius,
       this.radius,
+      0,
       Math.PI * 2,
       false
     );
     tooltip.fill();
+    tooltip.fillStyle = "black";
     tooltip.stroke();
+    tooltip.fillText(
+      `${this.vx} m/s`,
+      this.x - this.radius / 1.5,
+      canvas.height - this.radius,
+      2 * this.radius
+    );
     tooltip.closePath();
   }
 }
 
-const b1 = new Ball(1625, 10, 2, 30);
+const b1 = new Ball(350, 10, 2, 100);
 b1.draw();
-
-const b2 = new Ball(500, 0, 3, 30);
+const b2 = new Ball(150, 0, 3, 100);
 b2.draw();
